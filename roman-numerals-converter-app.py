5afb1faa4a7bee796afb53c6c2b47da2a452526f
paul-clarusway <babydevsecops@gmail.com>
2025-03-22 17:02:13 +0300

    update1

--- roman-numerals-converter-app.py
+++ roman-numerals-converter-app.py
@@ -44,5 +44,5 @@ def convert_html():
         return redirect(url_for("index"))
 
 if __name__=="__main__":
-    app.run(debug=True)
-    #app.run(host='0.0.0.0', port=8080)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
